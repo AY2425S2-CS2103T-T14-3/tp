b60913d740dcc3729147878a3c3cf6f539c81d59
simenggg <2841583540lsm@gmail.com>
2025-03-14 01:19:44 +0800

    Update delete feature
    Edit DeleteCommand, Person, ParseUtil and JsonAdaptedPerson classes
    
    Add ID class

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -9,6 +9,7 @@ import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.ID;
 import seedu.address.model.person.Person;
 
 /**
@@ -19,16 +20,16 @@ public class DeleteCommand extends Command {
     public static final String COMMAND_WORD = "delete";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the person identified by the index number used in the displayed person list.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + ": Deletes the student identified by the student id used in the displayed person list.\n"
+            + "Parameters: ID (must be in the format AxxxxxxxX, where x is a number and X is a capital letter)\n"
+            + "Example: " + COMMAND_WORD + " A0123456X";
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
 
-    private final Index targetIndex;
+    private final ID targetID;
 
-    public DeleteCommand(Index targetIndex) {
-        this.targetIndex = targetIndex;
+    public DeleteCommand(ID targetID) {
+        this.targetID = targetID;
     }
 
     @Override
@@ -36,11 +37,19 @@ public class DeleteCommand extends Command {
         requireNonNull(model);
         List<Person> lastShownList = model.getFilteredPersonList();
 
-        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+        /* don't need to check whether index is larger than the largest current index
+        if (targetID.getZeroBased() >= lastShownList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
+        */
 
-        Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
+        //adding a hashmap of people and id?
+        //Person personToDelete = personIdMap.get(targetID);
+        //original: Person personToDelete = lastShownList.get(targetID.);
+        Person personToDelete = lastShownList.stream()
+                        .filter(person -> person.getId().equals(targetID))
+                                .findFirst()
+                                        .orElse(null);
         model.deletePerson(personToDelete);
         return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(personToDelete)));
     }
@@ -57,13 +66,13 @@ public class DeleteCommand extends Command {
         }
 
         DeleteCommand otherDeleteCommand = (DeleteCommand) other;
-        return targetIndex.equals(otherDeleteCommand.targetIndex);
+        return targetID.equals(otherDeleteCommand.targetID);
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
-                .add("targetIndex", targetIndex)
+                .add("targetIndex", targetID)
                 .toString();
     }
 }
--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.tag.Tag;
 
@@ -20,6 +21,7 @@ public class Person {
     private final Name name;
     private final Phone phone;
     private final Email email;
+    private final ID id;
 
     // Data fields
     private final Address address;
@@ -28,11 +30,12 @@ public class Person {
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+    public Person(Name name, Phone phone, Email email, ID id, Address address, Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
+        this.id = id;
         this.address = address;
         this.tags.addAll(tags);
     }
@@ -47,6 +50,10 @@ public class Person {
 
     public Email getEmail() {
         return email;
+    }
+
+    public ID getId() {
+        return id;
     }
 
     public Address getAddress() {
@@ -93,6 +100,7 @@ public class Person {
         return name.equals(otherPerson.name)
                 && phone.equals(otherPerson.phone)
                 && email.equals(otherPerson.email)
+                && id.equals(otherPerson.id)
                 && address.equals(otherPerson.address)
                 && tags.equals(otherPerson.tags);
     }
@@ -100,7 +108,7 @@ public class Person {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
+        return Objects.hash(name, phone, email, id, address, tags);
     }
 
     @Override
@@ -109,6 +117,7 @@ public class Person {
                 .add("name", name)
                 .add("phone", phone)
                 .add("email", email)
+                .add("id", id)
                 .add("address", address)
                 .add("tags", tags)
                 .toString();
