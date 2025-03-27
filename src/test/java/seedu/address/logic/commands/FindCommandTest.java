0280b206533ace18cccc79e5e136fbdafe438c2e
simenggg <2841583540lsm@gmail.com>
2025-03-27 12:26:02 +0800

    Add test cases to test StudentIdMatchPredicate

--- src/test/java/seedu/address/logic/commands/FindCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindCommandTest.java
@@ -6,9 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.CARL;
+import static seedu.address.testutil.TypicalPersons.DANIEL;
 import static seedu.address.testutil.TypicalPersons.ELLE;
 import static seedu.address.testutil.TypicalPersons.FIONA;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalStudentIds.STUDENT_ID_SECOND_PERSON;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -19,6 +21,8 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.StudentId;
+import seedu.address.model.person.StudentIdMatchPredicate;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code FindCommand}.
@@ -75,10 +79,30 @@ public class FindCommandTest {
     }
 
     @Test
+    public void execute_find_by_studentId() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 1);
+        StudentIdMatchPredicate predicate = new StudentIdMatchPredicate(new StudentId("A1234567D"));
+        FindCommand command = new FindCommand(predicate);
+        expectedModel.updateFilteredPersonList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(DANIEL), model.getFilteredPersonList());
+
+    }
+
+
+    @Test
     public void toStringMethod() {
         NameContainsKeywordsPredicate predicate = new NameContainsKeywordsPredicate(Arrays.asList("keyword"));
         FindCommand findCommand = new FindCommand(predicate);
         String expected = FindCommand.class.getCanonicalName() + "{name predicate=" + predicate + "}";
+        assertEquals(expected, findCommand.toString());
+    }
+
+    @Test
+    public void toStringMethod_id() {
+        StudentIdMatchPredicate predicate = new StudentIdMatchPredicate(new StudentId("A1112222P"));
+        FindCommand findCommand = new FindCommand(predicate);
+        String expected = FindCommand.class.getCanonicalName() + "{id predicate=" + predicate + "}";
         assertEquals(expected, findCommand.toString());
     }
 
