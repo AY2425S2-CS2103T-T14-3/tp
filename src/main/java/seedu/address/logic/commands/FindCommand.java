30be5275349249c00b0fc179a63e8900b76d5064
simenggg <2841583540lsm@gmail.com>
2025-04-03 12:09:10 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -2,13 +2,13 @@ package seedu.address.logic.commands;
 
 import java.util.Objects;
 
+import static java.util.Objects.requireNonNull;
+
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.StudentIdMatchPredicate;
-
-import static java.util.Objects.requireNonNull;
 
 
 /**
